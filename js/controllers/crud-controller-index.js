ba33e8244169266fa977f81dafe8ee0e788f3527
gustavo <gustavo@logcarga.com>
2017-09-19 10:20:13 -0300

    exibindo segundo exemplo angularJS

--- js/controllers/crud-controller-index.js
+++ js/controllers/crud-controller-index.js
@@ -6,8 +6,11 @@ angular.module('crud.controllers.index',[],function() {
 // Registrando um controller para o modulo
 angular.module('crud.controllers.index').controller('crudController', function($scope) {
     
-    $scope.addItem = function(){
-        alert("ola");
+    $scope.nomeInformado;
+    $scope.resultado;
+    
+    $scope.informarNome = function(){
+        $scope.resultado = "Voce informou o nome: "+$scope.nomeInformado;
     }
     
 });
